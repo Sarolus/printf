1ecbb147d770e63fc2bfc0b6c0e7e8df753ac6f2
SimonBr017 <simonbrard017@gmail.com>
2021-03-13 14:20:00 +0000

    struct and prototype

--- holberton.h
+++ holberton.h
@@ -1,1 +1,15 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+typedef struct form
+{
+	char *form;
+	void (*f)(int, int);
+} form_t
+
 int _putchar(char c);
+int _printf(const char *format, ...);
+void print_char(va_list args);
+void print_str(va_list args);
+
+#endif
